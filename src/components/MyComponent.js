8edcae0fe604a2b76e6d6e4fc12b7f834bba7d50
DavidThompson-007 <haitrilehuu@gmail.com>
2025-03-18 21:30:39 +0700

    video-16

---
+++ src/components/MyComponent.js
@@ -0,0 +1,15 @@
+//class component
+//function component
+import React from 'react';
+class MyComponent extends React.Component {
+    render() {
+        return (
+            <div>
+                <div>
+                    my first component
+                </div>
+            </div>
+        )
+    }
+}
+export default MyComponent;
